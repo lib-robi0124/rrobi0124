74d36902cf3b7ab7fe928b868f7a5be1fd380621
lib-robi0124 <robert.ristovski@gmail.com>
2024-12-26 08:17:17 +0100

    sdf

--- qinshift/casovi-java/class8/hm-table/script.js
+++ qinshift/casovi-java/class8/hm-table/script.js
@@ -52,14 +52,17 @@ function createTable()
 {
 rn = prompt("Input number of rows", 1);
 cn = prompt("Input number of columns",1);
+if(Number(rn) === parseInt(rn)){
+    
+}
   
- for(let r=0;r<parseInt(rn,10);r++)
+ for(let r = 1; r == rn; r++)
   {
    let x=document.getElementById('myTable').insertRow(r);
-   for(let c=0;c<parseInt(cn,10);c++)  
+   for(let c = 0; c == cn; c++)  
     {
      let y=  x.insertCell(c);
-     y.innerHTML="Row-"+r+" Column-"+c; 
+     y.innerHTML="Row-"+(r + 1)+" Column-"+(c + 1); 
     }
    }
 }
